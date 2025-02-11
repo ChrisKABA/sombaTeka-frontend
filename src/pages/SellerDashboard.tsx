3e14a640c3fba6d2a84ea6df740710d7722921b4
chris <chriskaba@gmail.com>
2025-02-11 17:30:03 +0100

    add component ProductList and AddProduct v3

--- src/pages/SellerDashboard.tsx
+++ src/pages/SellerDashboard.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import DashboardSidebar, { MenuItem } from '../components/DashboardSidebar';
 import ProductsList from '../components/ProductsList';
 import { products } from '../components/mock/Products';
